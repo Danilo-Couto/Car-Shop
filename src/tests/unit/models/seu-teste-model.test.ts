0560801e6f064c6f0c6f9c0665950d414f2f5aed
Danilo Couto <couto.danilo95@gmail.com>
2022-07-08 11:57:50 -0300

    init test model

--- src/tests/unit/models/seu-teste-model.test.ts
+++ src/tests/unit/models/seu-teste-model.test.ts
@@ -1,2 +1,36 @@
-// import { expect } from 'chai';
-// import mongoose from 'mongoose';
+import { Model } from "mongoose";
+import { Car } from "../../../interfaces/CarInterface";
+import CarModel from "../../../models/car.model";
+
+const inserCar = {
+    model: 'Ferrari Maranello',
+    year: 1963,
+    color: 'red',
+    buyValue: 3500000,
+    seatsQty: 2,
+    doorsQty: 2
+};
+
+describe('Inser CarModel', ()=>{
+    let carModel: CarModel;
+    let mongooseModelMock = {
+        create: (car: any) => {
+            return {
+                ...car, 
+                _id: {
+                    $oid: 'id'
+                }
+            }
+        }
+    } as Model<Car>
+    before(()=>{
+        carModel = new CarModel();
+    })
+
+    describe('test create fucntion', ()=>{
+        it('retorna um objeto com _id', async() => {
+            const car = await carModel.create(inserCar);
+        expect(car).to.have.property('_id')
+        })
+    })
+})
