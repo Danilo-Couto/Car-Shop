b1a893d71d4b0cdffaaa56ac4311fb775953cf68
Danilo Couto <couto.danilo95@gmail.com>
2022-07-08 11:56:31 -0300

    feat car controllers

--- src/controllers/car.controller..ts
+++ src/controllers/car.controller..ts
@@ -27,15 +27,36 @@ export default class CarController extends Controller<Car> {
 
     try {
       const car = await this.service.create(body);
-      if (!car) {
-        return res.status(500)
-          .json({ error: this.errors.internal });
-      }
+
+      if (!car) return res.status(400).json({ error: this.errors.notFound });
       if ('error' in car) return res.status(400).json(car);
+     
       return res.status(201).json(car);
-    } catch (err) {
-      return res.status(500)
-        .json({ error: this.errors.internal }); 
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ error: this.errors.internal }); 
+    }
+  };
+
+  update = async (
+    req: RequestWithBody<Car>,
+    res: Response<Car | ResponseError>,
+  ): Promise<typeof res> => {
+    const { id } = req.params;
+
+    if (!id) return res.status(400).json({ error: this.errors.requiredId });
+
+    try {
+      const { body } = req;
+      const car = await this.service.update(id, body);
+
+      if (!car) return res.status(400).json({ error: this.errors.notFound });
+      if ('error' in car) return res.status(400).json(car);
+
+      return res.status(200).json(car);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ error: this.errors.internal });
     }
   };
 }
