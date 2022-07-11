3a6b3a6f451b49b3a45f6325339a5caa9e85745f
Danilo Couto <couto.danilo95@gmail.com>
2022-07-11 18:51:57 -0300

    feat test controller

--- src/tests/unit/controllers/car-controller.test.ts
+++ src/tests/unit/controllers/car-controller.test.ts
@@ -5,9 +5,8 @@ import { Request, Response } from 'express';
 import CarController from '../../../controllers/car.controller';
 import CarModel, { CarMongooseModel } from '../../../models/car.model';
 import CarService from '../../../services/car.service';
-import { carMock, validId } from '../mocks/carMocks';
+import { carMock, carMockUpdated, validId } from '../mocks/carMocks';
 import Sinon = require('sinon');
-import { afterEach, beforeEach } from 'mocha';
 
 chai.use(chaiHttp);
 
@@ -17,18 +16,20 @@ const carModel = new CarModel(CarMongooseModel);
 const carService = new CarService(carModel);
 const carController = new CarController(carService)
 
-const req = {} as Request;
-const res = {} as Response;
-
 describe('Tests Car Controller', () => {
-    beforeEach(async () => {
-        req.body = {carMock};
-        res.status = sinon.stub().returns(res);
-        res.json = sinon.stub().returns(res);   
-    });
-
     describe('Tests Create method', () => {
-    Sinon.stub(carService, 'create').resolves(carMock);
+        const req = {} as Request;
+        const res = {} as Response;
+
+        before(async () => {
+            Sinon.stub(carService, 'create').resolves(carMock);
+            // req.body = {carMock};
+            res.status = sinon.stub().returns(res);
+            res.json = sinon.stub().returns(res);   
+        });
+        after(() => {
+            (carService.create as sinon.SinonStub).restore();
+          });
 
       it('Success', async () => {
         await carController.create(req, res)
@@ -37,8 +38,18 @@ describe('Tests Car Controller', () => {
         });
     });
 
-    describe('Tests Read Method', () => {      
-        Sinon.stub(carService, 'read').resolves([carMock]);
+    describe('Tests Read Method', () => {     
+        const req = {} as Request;
+        const res = {} as Response;
+
+        before(async () => {
+            Sinon.stub(carService, 'read').resolves([carMock]);
+            res.status = sinon.stub().returns(res);
+            res.json = sinon.stub().returns(res);   
+        });    
+        after(() => {
+            (carService.read as sinon.SinonStub).restore();
+          }); 
 
         it('Success', async () => {
             await carController.read(req, res)
@@ -47,14 +58,64 @@ describe('Tests Car Controller', () => {
         });
     });
 
-    // describe('Tests ReadOne Method', () => {
-    //         Sinon.stub(carService, 'readOne').resolves(carMock);
-        
-    //     it('Success', async () => {
-    //         await carController.readOne(validId, res)
-    //         expect((res.status as sinon.SinonStub).calledWith(200)).to.be.true;
-    //         expect((res.json as sinon.SinonStub).calledWith([carMock])).to.be.true;
-    //     });
-    // });
+    describe('Tests ReadOne Method', () => {
+        const req = { params: { id: validId } } as any;
+        const res = {} as Response;
 
+        before(async () => {
+            Sinon.stub(carService, 'readOne').resolves(carMock);
+            res.status = sinon.stub().returns(res);
+            res.json = sinon.stub().returns(res);   
+        });
+        after(() => {
+            (carService.readOne as sinon.SinonStub).restore();
+          }); 
+        
+        it('Success', async () => {
+            await carController.readOne(req, res)
+            expect((res.status as sinon.SinonStub).calledWith(200)).to.be.true;
+            expect((res.json as sinon.SinonStub).calledWith(carMock)).to.be.true;
+        });
+    });
+
+    describe('Tests Update Method', () => {
+        const req = { params: { id: validId } } as any;
+        const res = {} as Response;
+        req.body = {carMockUpdated};
+
+        before(async () => {
+            Sinon.stub(carService, 'update').resolves(carMockUpdated);
+            res.status = sinon.stub().returns(res);
+            res.json = sinon.stub().returns(res);   
+        });
+        after(() => {
+            (carService.update as sinon.SinonStub).restore();
+          }); 
+        
+        it('Success', async () => {
+            await carController.update(req, res)
+            expect((res.status as sinon.SinonStub).calledWith(200)).to.be.true;
+            expect((res.json as sinon.SinonStub).calledWith(carMockUpdated)).to.be.true;
+        });
+    });
+
+    describe('Tests Delete Method', () => {
+        const req = { params: { id: validId } } as any;
+        const res = {} as Response;
+
+        before(async () => {
+            Sinon.stub(carService, 'delete').resolves(carMock);
+            res.status = sinon.stub().returns(res);
+            res.json = sinon.stub().returns(res);   
+        });
+        after(() => {
+            (carService.delete as sinon.SinonStub).restore();
+          }); 
+        
+        it('Success', async () => {
+            await carController.delete(req, res)
+            expect((res.status as sinon.SinonStub).calledWith(204)).to.be.true;
+            expect((res.json as sinon.SinonStub).calledWith(carMock)).to.be.true;
+        });
+    });
 })
